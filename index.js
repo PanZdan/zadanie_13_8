bcd800034995a2ad2d10cf5b3afb8373007e8342
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-01-13 20:35:51 +0100

    Changed indentation again again:)

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ var http = require('http');
 var fs = require('fs');
 var server = http.createServer();
 server.on('request', function(request, response) {
-		response.setHeader("Content-Type", "text/html; charset=utf-8");
-		if (request.method === 'GET' && request.url === '/') {
-				fs.readFile('./index.html', 'utf-8', function(err, data) {
-						if (err) throw err;
-						response.write(data);
-						response.end();
-				})
-		} else {
-				response.writeHead(200, {
-						"Content-Type": "image/png"
-				});
-				response.statusCode = 404;
-				fs.readFile('./pexels.png', 'binary', function(err, data) {
-						if (err) throw err;
-						response.write(data, "binary");
-						response.end();
-				});
-		}
+	response.setHeader("Content-Type", "text/html; charset=utf-8");
+	if (request.method === 'GET' && request.url === '/') {
+		fs.readFile('./index.html', 'utf-8', function(err, data) {
+			if (err) throw err;
+			response.write(data);
+			response.end();
+		})
+	} else {
+		response.writeHead(200, {
+			"Content-Type": "image/png"
+		});
+		response.statusCode = 404;
+		fs.readFile('./pexels.png', 'binary', function(err, data) {
+			if (err) throw err;
+			response.write(data, "binary");
+			response.end();
+		});
+	}
 });
 server.listen(8080);
